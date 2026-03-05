a8d80865baf24c09e6fa556fa104147b331b8397
seoyeon020 <ksheee7000@naver.com>
2026-03-05 10:13:29 +0900

    junit update

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,13 +1,31 @@
 package myspring.di.xml;
 
+//static import 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
+	ApplicationContext context;
+	
+	@BeforeEach
+	void init() {
+		//Container 객체생성 
+		context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
+	}
+	
 	@Test
 	void helloBean() {
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
+		Hello helloById = (Hello)context.getBean("hello");
+		Hello helloByType = context.getBean("hello", Hello.class);
+		
+		//Hello SpringBean이 Singleton 객체인지를 검증하기
+		System.out.println(helloById == helloByType);
+		
+		assertSame(helloById, helloByType);
+		
 	}
 
 }
