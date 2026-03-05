b42d6d3e8668a78f9a9f5e3b77193d1369eadeda
seoyeon020 <ksheee7000@naver.com>
2026-03-05 17:15:58 +0900

    javaconfig update

--- src/test/java/myspring/di/xml/config/HelloConfigTest.java
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -1,12 +1,34 @@
 package myspring.di.xml.config;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.annotation.Resource;
+
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = HelloConfig.class)
 public class HelloConfigTest {
+	@Autowired
+	Hello hello;
 	
+	@Resource(name="stringPrinter")
+	Printer printer;
+	
+	@Test
+	void hello() {
+		
+		assertEquals("Hello 자바컨피그", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 자바컨피그", printer.toString());
+		
+	}
 
 }
